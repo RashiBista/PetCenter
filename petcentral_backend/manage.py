78762266e60bb3b46928bc2a2f533e05ef1a7eff
Aditi <aditi.kc23@gmail.com>
2026-07-12 02:13:17 +0545

    create venv and update manage

--- petcentral_backend/manage.py
+++ petcentral_backend/manage.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
+
 import os
 import sys
-import django.core.management
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE",
+        "config.settings",
+    )
+
     try:
-        django.core.management.execute_from_command_line(sys.argv)
+        from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Make sure Django is installed "
+            "and your virtual environment is activated."
         ) from exc
-    django.core.management.execute_from_command_line(sys.argv)
+
+    execute_from_command_line(sys.argv)
 
 
 if __name__ == "__main__":
